2882943ace94af3a8d781f5f8240dee27d7e58a7
Eugene Trounev <eugene.trounev@gmail.com>
2016-04-27 23:24:28 -0400

    Move exporting into it's own service

---
+++ src/js/ExportService.js
@@ -0,0 +1,43 @@
+/**
+ * Angular.js application service
+ *
+ * @author eugene.trounev(a)gmail.com
+ */
+
+angular.module('app')
+    .factory('$exportSerice',[
+        '$location',     
+        '$colorSerice',
+        'normalizeColorBitFilter',
+            function($location, $colorSerice, normalizeColorBitFilter) {
+    
+                var gimpTemplate = "GIMP Palette\nName: pal-pal\nColumns: 3\n",
+                    svgTemplate = "<svg></svg>";
+
+                function GIMPPalette(pal){
+                    var o = pal.map(function(item){return $colorSerice(item).toRgb();});
+
+                }
+    
+                function _import(str) {
+                    return angular.isDefined(str) && str.split('-').map(function(c) {
+                        return '#' + c;
+                    });
+                }
+    
+                function _export(pal) {
+                    return {
+                        string: pal.join(", "),
+                        url: $location.absUrl().replace($location.path(), "") + "#/" + pal.join("-").replace(/#/g, ""),
+                        json: angular.toJson(pal),
+                        gimp: "",
+                        ps: "",
+                        svg: ""
+                    }
+                }
+    
+                return {
+                    'import': _import,
+                    'export': _export
+                };
+            }]);
